25787085e1a2ee6572364166c142578c0601ec4b
Volodymyr Forfutdinov <volodymyr_forfutdinov@epam.com>
2020-07-09 13:48:12 +0300

    - refactor to use more arguments; - add inteval update for signal;

--- modem-command.sh
+++ modem-command.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 BASE_URL=http://192.168.8.1
 TOKEN_PATH=/api/webserver/SesTokInfo
 CONTROL_PATH=/api/device/control
@@ -9,31 +11,62 @@ TOKEN_OUTPUT=$(curl --silent ${BASE_URL}${TOKEN_PATH})
 SESINFO_DATA=$(sed -n 's:.*<SesInfo>\(.*\)</SesInfo>.*:\1:p' <<< $TOKEN_OUTPUT)
 TOKEN_DATA=$(sed -n 's:.*<TokInfo>\(.*\)</TokInfo>.*:\1:p' <<< $TOKEN_OUTPUT)
 
-for i in "$@"
-do
-key="$1"
+DO_REBOOT=
+DO_SIGNAL=
+INTERVAL=0
 
-case $key in
-    -r|--reboot)
-      REQUEST_STRING="curl --silent ${BASE_URL}${CONTROL_PATH} --header 'Cookie: ${SESINFO_DATA}' --header '__RequestVerificationToken: $TOKEN_DATA' --data-raw '${REBOOT_CONTROL}'"
-      REBOOT_OUTPUT=`eval "$REQUEST_STRING"`
-      OUTPUT_MESSAGE=`sed -n 's:.*<response>\(.*\)</response>.*:\1:p' <<< $REBOOT_OUTPUT`
-      
-      echo "Response: ${OUTPUT_MESSAGE}"
-    ;;
-    -s|--signal)
-      REQUEST_STRING="curl --silent ${BASE_URL}${SIGNAL_PATH} --header 'Cookie: ${SESINFO_DATA}'"
-      SIGNAL_RESULT=`eval "$REQUEST_STRING"`
+getSignal()
+{
+  local REQUEST_STRING="curl --silent ${BASE_URL}${SIGNAL_PATH} --header 'Cookie: ${SESINFO_DATA}'"
+  local SIGNAL_RESULT=`eval "$REQUEST_STRING"`
 
-      RSSI=`sed -n 's:.*<rssi>\(.*\)</rssi>.*:\1:p' <<< $SIGNAL_RESULT`
-      RSRP=`sed -n 's:.*<rsrp>\(.*\)</rsrp>.*:\1:p' <<< $SIGNAL_RESULT`
-      RSRQ=`sed -n 's:.*<rsrq>\(.*\)</rsrq>.*:\1:p' <<< $SIGNAL_RESULT`
-      SINR=`sed -n 's:.*<sinr>\(.*\)</sinr>.*:\1:p' <<< $SIGNAL_RESULT`
+  local RSSI=`sed -n 's:.*<rssi>\(.*\)</rssi>.*:\1:p' <<< $SIGNAL_RESULT`
+  local RSRP=`sed -n 's:.*<rsrp>\(.*\)</rsrp>.*:\1:p' <<< $SIGNAL_RESULT`
+  local RSRQ=`sed -n 's:.*<rsrq>\(.*\)</rsrq>.*:\1:p' <<< $SIGNAL_RESULT`
+  local SINR=`sed -n 's:.*<sinr>\(.*\)</sinr>.*:\1:p' <<< $SIGNAL_RESULT`
 
-      echo "RSSI: ${RSSI}"
-      echo "RSRP: ${RSRP}"
-      echo "RSRQ: ${RSRQ}"
-      echo "SINR: ${SINR}"
-    ;;
-esac
+  echo "RSSI: ${RSSI} | RSRP: ${RSRP} | RSRQ: ${RSRQ} | SINR: ${SINR}"
+}
+
+reboot()
+{
+ REQUEST_STRING="curl --silent ${BASE_URL}${CONTROL_PATH} --header 'Cookie: ${SESINFO_DATA}' --header '__RequestVerificationToken: $TOKEN_DATA' --data-raw '${REBOOT_CONTROL}'"
+  REBOOT_OUTPUT=`eval "$REQUEST_STRING"`
+  OUTPUT_MESSAGE=`sed -n 's:.*<response>\(.*\)</response>.*:\1:p' <<< $REBOOT_OUTPUT`
+
+  echo "Response: ${OUTPUT_MESSAGE}"
+}
+
+while getopts 'rsi:' OPTION; do
+  case $OPTION in
+      r) DO_REBOOT=1;;
+      s) DO_SIGNAL=1;;
+      i) INTERVAL=${OPTARG};;
+  esac
 done
+
+if [[ -n "$DO_REBOOT" ]]; then
+  echo "Reboot"
+  if [[ "$INTERVAL" != 0 ]]; then
+    echo "Reboot cannot be started with interval"
+    exit 0
+  fi
+
+ reboot
+fi
+
+if [[ -n "$DO_SIGNAL" ]]; then
+  echo "Signal"
+  
+  if [[ "$INTERVAL" != 0 ]]; then
+    while true
+    do
+      CURRENT_TIME=$(date +"%T")
+      RESULT=$(getSignal)
+      echo -ne "${CURRENT_TIME} - ${RESULT}"\\r
+      sleep $INTERVAL;
+    done
+  fi
+
+  getSignal
+fi
